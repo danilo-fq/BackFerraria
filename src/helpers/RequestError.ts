4154f5b3bf7c0cf29b10b7b533fff0bb4d7ca54c
Danilo Ferreira <danilof.queiroz1@gmail.com>
2022-09-13 18:45:20 -0300

    fix: RequestError class extends an Error to be throw an exception

--- src/helpers/RequestError.ts
+++ src/helpers/RequestError.ts
@@ -1,9 +1,10 @@
-class RequestError {
+class RequestError extends Error {
   statusCode: number;
 
   message: string;
 
   constructor(statusCode: number, message: string) {
+    super();
     this.message = message;
     this.statusCode = statusCode;
   }
