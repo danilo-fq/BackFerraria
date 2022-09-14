1978858feb40a0177f90810e411408e66f6b10e2
Danilo Ferreira <danilof.queiroz1@gmail.com>
2022-09-14 10:48:51 -0300

    fix: change export default to export functions and remove Character type to User interface

--- src/helpers/JwtServices.ts
+++ src/helpers/JwtServices.ts
@@ -1,17 +1,12 @@
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import Iuser from '../interfaces/user.interface';
 
 dotenv.config();
 
 const secret: jwt.Secret = process.env.JWT_SECRET || 'smith123';
 
-type Character = {
-  username:string,
-  classe:string,
-  level:number,
-};
-
-const generateToken = (character: Character): string => {
+export const generateToken = (character: Iuser): string => {
   const token: string = jwt.sign({ data: character }, secret, {
     expiresIn: '30min',
     algorithm: 'HS256',
@@ -19,16 +14,11 @@ const generateToken = (character: Character): string => {
   return token;
 };
 
-const validateToken = (token: string): string | JwtPayload | null => {
+export const validateToken = (token: string): string | JwtPayload | null => {
   try {
     const decode: string | JwtPayload = jwt.verify(token, secret);
     return decode;
   } catch (err) {
     return null;
   }
-};
-
-export default {
-  generateToken,
-  validateToken,
 };
